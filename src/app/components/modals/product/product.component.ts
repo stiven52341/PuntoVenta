e885cc92d056fede8c0a45eb0a35b41a79609277
stiven52341 <sbuenoreynoso@outlook.es>
2025-03-04 21:45:12 -0400

    Product-component 2

--- src/app/components/modals/product/product.component.ts
+++ src/app/components/modals/product/product.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonContent, IonHeader } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTabButton, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { HeaderBarComponent } from '../../header-bar/header-bar.component';
 import { IProduct } from 'src/app/models/product.model';
+import { addIcons } from 'ionicons';
+import { heart, shareSocial } from 'ionicons/icons';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
   standalone: true,
-  imports: [IonHeader, HeaderBarComponent, IonContent]
+  imports: [IonIcon, IonButton, IonButton, IonHeader, HeaderBarComponent, IonContent]
 })
 export class ProductComponent  implements OnInit {
   @Input({required:true}) product!: IProduct;
   @Input() image?: string;
 
-  constructor() { }
+  constructor() {
+    addIcons({heart, shareSocial});
+  }
 
   ngOnInit() {}
 
